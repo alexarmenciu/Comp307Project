d0f8e635ca64d12dacb560fb1968ce283aeed22b
Alex Armenciu <alexandru.armenciu@mail.mcgill.ca>
2023-12-05 02:37:58 -0500

    add signup functionality

--- frontend/src/app/components/header.tsx
+++ frontend/src/app/components/header.tsx
@@ -1,7 +1,45 @@
+"use client";
 import React from "react";
 import styles from "../page.module.css";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [hasSession, setHasSession] = useState(false);
+  const [username, setUsername] = useState("");
+  const logout = async () => {
+    document.cookie = "session=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+    document.cookie = "email=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+    window.location.href = "/";
+  };
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const sessionCookie = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("session="));
+
+      if (sessionCookie) {
+        const expirationTimeString = sessionCookie.split("=")[1];
+        const expirationDate = new Date(expirationTimeString);
+
+        if (
+          !isNaN(expirationDate.getTime()) &&
+          expirationDate.getTime() > Date.now()
+        ) {
+          // Session is still valid
+          setHasSession(true);
+          setUsername(
+            document.cookie
+              .split("; ")
+              .find((row) => row.startsWith("email="))
+              .split("=")[1]
+          );
+        }
+      }
+    };
+
+    checkSession();
+  }, []);
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
@@ -19,9 +57,15 @@ export default function Header() {
           <a href="/about" className={styles.link}>
             About
           </a>
-          <a href="/login" className={styles.link}>
-            Login
-          </a>
+          {hasSession ? (
+            <a className={styles.link} onClick={logout}>
+              {username}
+            </a>
+          ) : (
+            <a href="/signup" className={styles.link}>
+              Login
+            </a>
+          )}
         </div>
       </nav>
     </header>
---
+++ frontend/src/app/signup/page.tsx
@@ -0,0 +1,83 @@
+"use client";
+// pages/login.tsx
+import { useState } from "react";
+import styles from "../page.module.css";
+import Header from "../components/header";
+
+export default function SignupPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("here");
+
+    try {
+      const response = await fetch("http://localhost:3001/db/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        const date = new Date();
+        date.setDate(date.getDate() + 7);
+
+        // Note: Modifying document.cookie is a client-side operation
+        document.cookie = `session=${date.toUTCString()}`;
+        document.cookie = `email=${email}`;
+
+        // Redirecting on the client side
+        window.location.href = "/";
+      } else {
+        alert("Login failed.");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <Header />
+      <main className={styles.main}>
+        <h1 className={styles.title}>Sign Up</h1>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <div className={styles.field}>
+            <label htmlFor="email" className={styles.label}>
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              className={styles.input}
+              required
+            />
+          </div>
+          <div className={styles.field}>
+            <label htmlFor="password" className={styles.label}>
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              className={styles.input}
+              required
+            />
+          </div>
+          <div className={styles.field}>
+            <button type="submit" className={styles.button}>
+              Login
+            </button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
+}
