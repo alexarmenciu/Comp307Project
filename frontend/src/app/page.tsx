90c54bec8a135c87c2aac6c825ad53d9fccbda47
Susan Pan <144385544+suusanp@users.noreply.github.com>
2023-12-07 21:16:01 -0500

    fixed error for real

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -14,31 +14,27 @@ export default function Home() {
         <div className={styles.description}>
           <div className={styles.center} style={{ width: "80vw" }}>
             <section>
-              <p>
-                <h2 className={styles.title} style={{ fontSize: "7vh" }}>
-                  Our Successes
-                </h2>
-                <div
-                  style={{ display: "flex", justifyContent: "space-between" }}
-                >
-                  <div style={{ padding: "20px" }}>
-                    <h3>Jobs Posted</h3>
-                    <img
-                      src="https://www.aeaweb.org/content/file?id=4578"
-                      style={{ height: "20vh" }}
-                    />
-                    <p>1000+</p>
-                  </div>
-                  <div style={{ padding: "20px" }}>
-                    <h3>Happy Hires</h3>
-                    <img
-                      src="https://assets-global.website-files.com/6516813912555e8b460c1f1e/652d1a8b12eb4cf0c38e2af8_Unique%20hiring%20process.png"
-                      style={{ height: "20vh" }}
-                    />
-                    <p>500+</p>
-                  </div>
+              <h2 className={styles.title} style={{ fontSize: "7vh" }}>
+                Our Successes
+              </h2>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <div style={{ padding: "20px" }}>
+                  <h3>Jobs Posted</h3>
+                  <img
+                    src="https://www.aeaweb.org/content/file?id=4578"
+                    style={{ height: "20vh" }}
+                  />
+                  <p>1000+</p>
                 </div>
-              </p>
+                <div style={{ padding: "20px" }}>
+                  <h3>Happy Hires</h3>
+                  <img
+                    src="https://assets-global.website-files.com/6516813912555e8b460c1f1e/652d1a8b12eb4cf0c38e2af8_Unique%20hiring%20process.png"
+                    style={{ height: "20vh" }}
+                  />
+                  <p>500+</p>
+                </div>
+              </div>
             </section>
           </div>
         </div>
