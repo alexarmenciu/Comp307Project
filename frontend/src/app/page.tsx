f4a39752220e67030c60626da388a45fffb50bde
Susan Pan <144385544+suusanp@users.noreply.github.com>
2023-12-07 21:13:38 -0500

    fixed homepage errors finally

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -12,31 +12,35 @@ export default function Home() {
           Welcome to Hacker Jobs
         </h1>
         <div className={styles.description}>
-          <p className={styles.center} style={{ width: "80vw" }}>
+          <div className={styles.center} style={{ width: "80vw" }}>
             <section>
-              <h2 className={styles.title} style={{ fontSize: "7vh" }}>
-                Our Successes
-              </h2>
-              <div style={{ display: "flex", justifyContent: "space-between" }}>
-                <div style={{ padding: "20px" }}>
-                  <h3>Jobs Posted</h3>
-                  <img
-                    src="https://www.aeaweb.org/content/file?id=4578"
-                    style={{ height: "20vh" }}
-                  />
-                  <p>1000+</p>
+              <p>
+                <h2 className={styles.title} style={{ fontSize: "7vh" }}>
+                  Our Successes
+                </h2>
+                <div
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <div style={{ padding: "20px" }}>
+                    <h3>Jobs Posted</h3>
+                    <img
+                      src="https://www.aeaweb.org/content/file?id=4578"
+                      style={{ height: "20vh" }}
+                    />
+                    <p>1000+</p>
+                  </div>
+                  <div style={{ padding: "20px" }}>
+                    <h3>Happy Hires</h3>
+                    <img
+                      src="https://assets-global.website-files.com/6516813912555e8b460c1f1e/652d1a8b12eb4cf0c38e2af8_Unique%20hiring%20process.png"
+                      style={{ height: "20vh" }}
+                    />
+                    <p>500+</p>
+                  </div>
                 </div>
-                <div style={{ padding: "20px" }}>
-                  <h3>Happy Hires</h3>
-                  <img
-                    src="https://assets-global.website-files.com/6516813912555e8b460c1f1e/652d1a8b12eb4cf0c38e2af8_Unique%20hiring%20process.png"
-                    style={{ height: "20vh" }}
-                  />
-                  <p>500+</p>
-                </div>
-              </div>
+              </p>
             </section>
-          </p>
+          </div>
         </div>
       </main>
       <footer className={styles.footer}>
