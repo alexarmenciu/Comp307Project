2554303eb382e3e8b1d1cd460dac53d47ac396bd
Alex Armenciu <alexandru.armenciu@mail.mcgill.ca>
2023-12-10 00:03:27 -0500

    see if fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = {
+  apps: [
+    {
+      name: "hackerjobs",
+      script: "./node_modules/next/dist/bin/next",
+      args: "start -p " + (process.env.PORT || 3000),
+      watch: false,
+      autorestart: true,
+    },
+  ],
+};
